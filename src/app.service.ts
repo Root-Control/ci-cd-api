8ac7e80542c58cf0e22146de20f3d7eaed25d8b3
HiroPalacios <l2oot.control@gmail.com>
2020-11-29 22:48:02 -0500

    installing depdendencies before building

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+let serverUpDate = new Date();
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World From nestjs!';
+    return `Hello World From nestjs! serverUp = ${serverUpDate}`;
   }
 }

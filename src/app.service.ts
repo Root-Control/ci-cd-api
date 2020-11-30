2b864680f4dcd74c7b66e23e4e83920dbbdd647d
HiroPalacios <l2oot.control@gmail.com>
2020-11-29 22:59:52 -0500

    Proc

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ let serverUpDate = new Date();
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World From nestjs! serverUp = ${serverUpDate}`;
+    return `Hello World From nestjs! serverUp  = ${serverUpDate}`;
   }
 }

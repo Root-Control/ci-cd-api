057ed516c1af87879b0855f5d5464cbbe9b32c09
HiroPalacios <l2oot.control@gmail.com>
2020-12-03 11:52:38 -0500

    adding sse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse, MessageEvent } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Observable, interval } from 'rxjs';
+import { map  } from 'rxjs/operators';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('sse')
+  sse(): Observable<MessageEvent> {
+    return interval(1000).pipe(map((_) => ({ data: { hello: 'world' } })));
   }
 }
